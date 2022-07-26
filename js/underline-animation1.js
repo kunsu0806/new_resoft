2341a2018d5bb3a0515f55bee866e2f94aada9e4
kunsu0806 <88035541+kunsu0806@users.noreply.github.com>
2022-07-26 09:25:02 +0900

    0726 update
    index page done

---
+++ js/underline-animation1.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+    $(".one-cl").addClass("color");
+  //   First active item
+    $(".btn-one").click(function(){
+       $(".bottom-line").addClass("bottom-active");
+       $(".one-cl").addClass("color");
+       $(".two-cl").removeClass("color1");
+       $(".three-cl").removeClass("color2");
+       $(".bottom-line").removeClass("bottom-active1 bottom-active2");
+    });
+    
+    //  Second active item
+    $(".btn-two").click(function(){
+        $(".bottom-line").addClass("bottom-active1");
+        $(".two-cl").addClass("color1");
+        $(".one-cl").removeClass("color");
+        $(".three-cl").removeClass("color2");
+        $(".bottom-line").removeClass("bottom-active bottom-active2");
+    });
+    
+    // Third active item
+     $(".btn-three").click(function(){
+          $(".bottom-line").addClass("bottom-active2");
+          $(".three-cl").addClass("color2");
+          $(".one-cl").removeClass("color");
+          $(".two-cl").removeClass("color1");
+          $(".bottom-line").removeClass("bottom-active bottom-active1");
+    });
+  });
