fc149378941f0e953fbb1ad6f616ed9071a2d4c3
kunsu0806 <88035541+kunsu0806@users.noreply.github.com>
2022-08-23 14:58:31 +0900

    0823 update

--- js/load.js
+++ js/load.js
@@ -1,3 +1,133 @@
+/* if (window.matchMedia("(min-width: 1281px)").matches) {
+    $(function () {
+        $(".card").hide();
+        var array = [];
+        var list = document.getElementsByClassName('card');
+
+        for (var i = 0; i < list.length; i++) {
+            array.push(list[i]);
+        }
+        var visiArray = [];
+        visiArray = array.slice(0, 6);
+
+        for (var i = 0; i < visiArray.length; i++) {
+            visiArray[i].style.display = 'block';
+        }
+        var infoHight = document.getElementById('infomation');
+        var load = document.getElementsByClassName('button')[0];
+
+        load.addEventListener('click', function (e) {
+            e.preventDefault();
+            var hiddenArray = [];
+            hiddenArray = array.slice(6, 10);
+
+            for (var i = 0; i < hiddenArray.length; i++) {
+                hiddenArray[i].style.display = 'block';
+            }
+            load.style.display = 'none';
+            
+        });
+    });
+}
+
+if (window.matchMedia("(min-width: 769px) and (max-width: 1280px)").matches) {
+    $(function () {
+        $(".card").hide();
+        var array = [];
+        var list = document.getElementsByClassName('card');
+
+        for (var i = 0; i < list.length; i++) {
+            array.push(list[i]);
+        }
+        var visiArray = [];
+        visiArray = array.slice(0, 6);
+
+        for (var i = 0; i < visiArray.length; i++) {
+            visiArray[i].style.display = 'block';
+        }
+        var infoHight = document.getElementById('infomation');
+        var load = document.getElementsByClassName('button')[0];
+
+        load.addEventListener('click', function (e) {
+            e.preventDefault();
+            var hiddenArray = [];
+            hiddenArray = array.slice(6, 10);
+
+            for (var i = 0; i < hiddenArray.length; i++) {
+                hiddenArray[i].style.display = 'block';
+            }
+            load.style.display = 'none';
+            
+        });
+    });
+}
+
+if (window.matchMedia("(max-width: 768px)").matches) {
+    $(function () {
+        $(".card").hide();
+        var array = [];
+        var list = document.getElementsByClassName('card');
+
+        for (var i = 0; i < list.length; i++) {
+            array.push(list[i]);
+        }
+        var visiArray = [];
+        visiArray = array.slice(0, 2);
+
+        for (var i = 0; i < visiArray.length; i++) {
+            visiArray[i].style.display = 'block';
+        }
+        var infoHight = document.getElementById('infomation');
+        var load = document.getElementsByClassName('button')[0];
+        var count = 0
+
+        load.addEventListener('click', function (e) {
+            e.preventDefault();
+            var hiddenArray = [];
+            console.log(count);
+
+            if(count == 0){
+                hiddenArray = array.slice(2, 4);
+
+                for (var i = 0; i < hiddenArray.length; i++) {
+                    hiddenArray[i].style.display = 'block';
+                    count += 1;
+                }
+                
+            }
+            else if(count == 2){
+                hiddenArray = array.slice(4, 6);
+
+                for (var i = 0; i < hiddenArray.length; i++) {
+                    hiddenArray[i].style.display = 'block';
+                    count += 1;
+                }
+                
+            }
+            else if(count == 4){
+                hiddenArray = array.slice(6, 8);
+
+                for (var i = 0; i < hiddenArray.length; i++) {
+                    hiddenArray[i].style.display = 'block';
+                    count += 1;
+                }
+        
+            }
+            else if(count == 6){
+                hiddenArray = array.slice(8, 10);
+
+                for (var i = 0; i < hiddenArray.length; i++) {
+                    hiddenArray[i].style.display = 'block';
+                    count += 1;
+                }
+                
+                load.style.display = 'none';
+            }
+        });
+    });
+}
+*/
+
 if (window.matchMedia("(min-width: 769px)").matches) {
     $(function () {
         $(".card").hide();
